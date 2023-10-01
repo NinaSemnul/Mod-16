0da68aab09593c6e74ae874efd862b9ea411344c
NinaSemnul <bacardi.jewel@gmail.com>
2023-10-01 05:28:47 +0300

    Initial commit

---
+++ src/test/java/CalculatorTests.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
+public class CalculatorTests {
+
+    private SumCalculator calc;
+
+    @BeforeEach
+    public void beforeEach() {
+        calc = new SumCalculator();
+    }
+
+//    @Test
+//    public void testThatSumWorksCorrect1() {
+//        int actual = calc.sum(1);
+//        int expected = 1;
+//        Assertions.assertEquals(expected, actual);
+//    }
+
+    @Test
+    public void testThatSubWorksCorrect3() {
+        int actual = calc.sum(3);
+        int expected = 6;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testThatSubWorksCorrect0() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            calc.sum(0);
+        });
+    }
+
+}
